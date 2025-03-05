3ab88c8a74b87ff8728264818ce310a59e5b6bed
Muhammad Ahmad <ahmadwebcrafts@gmail.com>
2025-03-06 01:30:31 +0500

    add cta of job-seeker to have connected to required form

--- components/pages/TalentOutsourcing/HireTopTalent.tsx
+++ components/pages/TalentOutsourcing/HireTopTalent.tsx
@@ -1,10 +1,13 @@
+'use client';
 import FadeInBottom from '@/components/framermotion/FadeInBottom'
+import { usePathname } from 'next/navigation';
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 function HireTopTalent() {
+        const pathname = usePathname();
     return (
         <div className='container my-10'>
             <FadeInBottom once={false} distance={60} duration={0.7}>
@@ -16,7 +19,7 @@ function HireTopTalent() {
                 <div className='flex flex-col justify-center '>
                     <h3 className='text-black text-2xl font-bold text-center md:text-start'>Ready to Hire Top Talent?</h3>
                     <p className='text-base max-w-[450px] text-center md:text-start'>Tell us your needs, and we'll match you with the perfect candidate.</p>
-                    <Link href={'/get-started'} className='self-center md:self-start'>
+                    <Link href={pathname=='/job-seekers' ? "/application-form" : "/get-started"} className='self-center md:self-start'>
                         <Button className='self-center md:self-start w-[200px] mt-3 bg-white border-[1.4px] border-black px-3 py-1 rounded-lg text-black hover:bg-black hover:text-white' size={'lg'}>Get Started</Button>
                     </Link>
                 </div>
