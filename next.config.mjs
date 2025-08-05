304aed3e922fc6542708326d9a6a5c5fbf999af9
Ahmad <ahmadwebcrafts@gmail.com>
2025-08-06 00:02:51 +0500

    added cursor suggessions 5-aug-2025

--- next.config.mjs
+++ next.config.mjs
@@ -1,46 +1,50 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    trailingSlash: false,
-    async redirects() {
-        return [];
+  trailingSlash: false,
+  async redirects() {
+      return [];
+  },
+  async headers() {
+      return [
+          {
+              source: '/(.*)',
+              headers: [
+                  {
+                      key: 'X-Robots-Tag',
+                      value: 'index, follow'
+                  },
+                  {
+                      key: 'Cache-Control',
+                      value: 'public, max-age=3600'
+                  }
+              ],
+          },
+      ];
+  },
+  images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'via.placeholder.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'images.unsplash.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'res.cloudinary.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'blog.beyondhut.com',
+        },
+        {
+          protocol: 'https',
+          hostname: 'webwork.store',
+        },
+      ],
     },
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'X-Robots-Tag',
-                        value: 'index, follow'
-                    },
-                    {
-                        key: 'Cache-Control',
-                        value: 'public, max-age=3600'
-                    }
-                ],
-            },
-        ];
-    },
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'via.placeholder.com',
-          },
-          {
-            protocol: 'https',
-            hostname: 'images.unsplash.com',
-          },
-          {
-            protocol: 'https',
-            hostname: 'res.cloudinary.com',
-          },
-          {
-            protocol: 'https',
-            hostname: 'blog.beyondhut.com',
-          },
-        ],
-      },
 };
 
 export default nextConfig;
